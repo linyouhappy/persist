7baded72afbd8843a7247ef0c4215c8e3fb326f5
xiaxin <xia.xin.1988.07.28@163.com>
2013-10-23 21:26:23 +0800

    hello world

--- src/persist.c
+++ src/persist.c
@@ -1,17 +1,7 @@
-/*
- ============================================================================
- Name        : persist.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!");
 	return EXIT_SUCCESS;
 }
