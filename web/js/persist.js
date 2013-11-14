ef725704d1e936c7fb1727ea0dd36c21b875674b
xiaxin <xia.xin.1988.07.28@163.com>
2013-11-14 12:50:52 +0800

    add web dir

---
+++ web/js/persist.js
@@ -0,0 +1,40 @@
+function ws(address, flags) {
+	this.ws = null;
+	this.st = null;
+	this.id = null;
+	
+	var prefix = 'ws://';
+	if (undefined === flags) {
+		var address = prefix + address;
+	}
+	this.address = address;
+}
+
+ws.prototype.connect  = function() {
+	this.ws = new WebSocket(this.address);
+	
+	this.ws.data = this;
+	
+	this.ws.onopen  = this._open;
+	this.ws.onclose = this._close;
+	
+	this.ws.onerror = this._error;
+	this.ws.onmessage = this._message;
+}
+
+ws.prototype.set      = function(object) {
+	this.id = '#'+object.id;  //  信息输出ID
+}
+
+ws.prototype._open    = function(evt) { }
+ws.prototype._close   = function(evt) { }
+ws.prototype._error   = function(evt) {
+	var id = this.data.id;
+	if (null != id) {
+		$(id).html('Error occured: ' + event.data); 
+	} else {
+		console.log('Error occured: ' + event.data);
+	}
+}
+
+ws.prototype._message = function(evt) { }
